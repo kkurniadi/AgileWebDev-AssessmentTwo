1f20bb8479c5c22d6eab96f096098076aa279de9
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2022-09-06 14:29:22 +0800

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- Kirsten Kurniadi, ID: 30045816
+Assessment Task Two (Individual Project)
+Contact Page -->
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Contact | Assessment Two</title>
+	<!-- Bootstrap CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+
+<body>
+	<?php include_once('inc_nav.php'); ?>
+	<div class="container mx-5 my-3">
+		<h1>Contact</h1>
+	</div>
+	<div class="container mx-5 my-3">
+		<p>CITEMS</p>
+		<a class="btn btn-primary" href="https://www.citems.com.au/" role="button">Website</a>
+	</div>
+	<div class="container mx-5 my-3">
+		<p>SMTAFE</p>
+		<a class="btn btn-primary" href="https://www.southmetrotafe.wa.edu.au/" role="button">Website</a>
+	</div>
+	<!-- JavaScript Bundle with Popper -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
+		crossorigin="anonymous"></script>
+</body>
+
+</html>
