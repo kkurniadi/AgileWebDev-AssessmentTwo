a9bf80768d810ce5c96a898df9b82190187c34f6
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2022-09-06 12:41:36 +0800

    Update index.php
    Added functionality to page

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- Kirsten Kurniadi, ID: 30045816
+Assessment Task Two (Individual Project)
+Home Page -->
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Home | Assessment Two</title>
+	<!-- Bootstrap CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+
+<body>
+	<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-primary">
+		<div class="container-fluid">
+			<button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarCollapse">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a href="#" class="nav-link active">Home</a>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" role="button"
+							data-bs-toggle="dropdown">Questions</a>
+						<ul class="dropdown-menu">
+							<li><a href="questions/question1.html" class="dropdown-item">Question 1</a></li>
+							<li><a href="questions/question2.html" class="dropdown-item">Question 2</a></li>
+							<li><a href="questions/question3.html" class="dropdown-item">Question 3</a></li>
+							<li><a href="questions/question4.html" class="dropdown-item">Question 4</a></li>
+							<li><a href="questions/question5.html" class="dropdown-item">Question 5</a></li>
+							<li><a href="questions/question6.html" class="dropdown-item">Question 6</a></li>
+							<li><a href="questions/question7.html" class="dropdown-item">Question 7</a></li>
+							<li><a href="questions/question8.html" class="dropdown-item">Question 8</a></li>
+							<li><a href="questions/question9.html" class="dropdown-item">Question 9</a></li>
+						</ul>
+					</li>
+					<li class="nav-item">
+						<a href="contact.html" class="nav-link">Contact</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<div class="container mx-5 my-3">
+		<h1>Welcome to HTML/CSS/JS and Bootstrap 5</h1>
+	</div>
+	<div class="container bg-primary text-white mt-4 mx-5 ps-5 py-5">
+		<p>Agile Web Development<br>
+			Kirsten Kurniadi<br>
+			30045816<br>
+			Assessment Two</p>
+	</div>
+	<!-- JavaScript Bundle with Popper -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
+		crossorigin="anonymous"></script>
+</body>
+
+</html>
