5373044b53dc4ae39bdc8361e2b6364165aa48b8
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2022-09-06 12:59:51 +0800

    Move navbar to separate file

---
+++ inc_nav.php
@@ -0,0 +1,32 @@
+<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-primary">
+	<div class="container-fluid">
+		<button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarCollapse">
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a href="#" class="nav-link active">Home</a>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" role="button"
+						data-bs-toggle="dropdown">Questions</a>
+					<ul class="dropdown-menu">
+						<li><a href="questions/question1.html" class="dropdown-item">Question 1</a></li>
+						<li><a href="questions/question2.html" class="dropdown-item">Question 2</a></li>
+						<li><a href="questions/question3.html" class="dropdown-item">Question 3</a></li>
+						<li><a href="questions/question4.html" class="dropdown-item">Question 4</a></li>
+						<li><a href="questions/question5.html" class="dropdown-item">Question 5</a></li>
+						<li><a href="questions/question6.html" class="dropdown-item">Question 6</a></li>
+						<li><a href="questions/question7.html" class="dropdown-item">Question 7</a></li>
+						<li><a href="questions/question8.html" class="dropdown-item">Question 8</a></li>
+						<li><a href="questions/question9.html" class="dropdown-item">Question 9</a></li>
+					</ul>
+				</li>
+				<li class="nav-item">
+					<a href="contact.html" class="nav-link">Contact</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
--- index.php
+++ index.php
@@ -14,38 +14,7 @@ Home Page -->
 </head>
 
 <body>
-	<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-primary">
-		<div class="container-fluid">
-			<button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarCollapse">
-				<ul class="navbar-nav">
-					<li class="nav-item">
-						<a href="#" class="nav-link active">Home</a>
-					</li>
-					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" href="#" role="button"
-							data-bs-toggle="dropdown">Questions</a>
-						<ul class="dropdown-menu">
-							<li><a href="questions/question1.html" class="dropdown-item">Question 1</a></li>
-							<li><a href="questions/question2.html" class="dropdown-item">Question 2</a></li>
-							<li><a href="questions/question3.html" class="dropdown-item">Question 3</a></li>
-							<li><a href="questions/question4.html" class="dropdown-item">Question 4</a></li>
-							<li><a href="questions/question5.html" class="dropdown-item">Question 5</a></li>
-							<li><a href="questions/question6.html" class="dropdown-item">Question 6</a></li>
-							<li><a href="questions/question7.html" class="dropdown-item">Question 7</a></li>
-							<li><a href="questions/question8.html" class="dropdown-item">Question 8</a></li>
-							<li><a href="questions/question9.html" class="dropdown-item">Question 9</a></li>
-						</ul>
-					</li>
-					<li class="nav-item">
-						<a href="contact.html" class="nav-link">Contact</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</nav>
+	<?php include_once('inc_nav.php');?>
 	<div class="container mx-5 my-3">
 		<h1>Welcome to HTML/CSS/JS and Bootstrap 5</h1>
 	</div>
