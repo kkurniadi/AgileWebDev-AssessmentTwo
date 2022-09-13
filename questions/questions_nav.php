9dab8c5b32efa1d5b55797c168865d07880e82f0
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2022-09-13 12:34:59 +0800

    Update questions_nav.php
    Fix navbar links

--- questions/questions_nav.php
+++ questions/questions_nav.php
@@ -6,25 +6,25 @@
 		<div class="collapse navbar-collapse" id="navbarCollapse">
 			<ul class="navbar-nav">
 				<li class="nav-item">
-					<a href="index.php" class="nav-link">Home</a>
+					<a href="..\index.php" class="nav-link">Home</a>
 				</li>
 				<li class="nav-item dropdown">
 					<a class="nav-link dropdown-toggle" href="#" role="button"
 						data-bs-toggle="dropdown" aria-expanded="false">Questions</a>
 					<ul class="dropdown-menu">
-						<li><a href="questions/question1.php" class="dropdown-item">Question 1</a></li>
-						<li><a href="questions/question2.php" class="dropdown-item">Question 2</a></li>
-						<li><a href="questions/question3.php" class="dropdown-item">Question 3</a></li>
-						<li><a href="questions/question4.php" class="dropdown-item">Question 4</a></li>
-						<li><a href="questions/question5.php" class="dropdown-item">Question 5</a></li>
-						<li><a href="questions/question6.php" class="dropdown-item">Question 6</a></li>
-						<li><a href="questions/question7.php" class="dropdown-item">Question 7</a></li>
-						<li><a href="questions/question8.php" class="dropdown-item">Question 8</a></li>
-						<li><a href="questions/question9.php" class="dropdown-item">Question 9</a></li>
+						<li><a href="question1.php" class="dropdown-item">Question 1</a></li>
+						<li><a href="question2.php" class="dropdown-item">Question 2</a></li>
+						<li><a href="question3.php" class="dropdown-item">Question 3</a></li>
+						<li><a href="question4.php" class="dropdown-item">Question 4</a></li>
+						<li><a href="question5.php" class="dropdown-item">Question 5</a></li>
+						<li><a href="question6.php" class="dropdown-item">Question 6</a></li>
+						<li><a href="question7.php" class="dropdown-item">Question 7</a></li>
+						<li><a href="question8.php" class="dropdown-item">Question 8</a></li>
+						<li><a href="question9.php" class="dropdown-item">Question 9</a></li>
 					</ul>
 				</li>
 				<li class="nav-item">
-					<a href="contact.php" class="nav-link">Contact</a>
+					<a href="..\contact.php" class="nav-link">Contact</a>
 				</li>
 			</ul>
 		</div>
